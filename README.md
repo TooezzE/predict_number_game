27c24b9bfebc938ea4d90aac9fe3e6a016c0ec6f
TooezzE <azazaz.com534@gmail.com>
2024-09-16 22:22:49 +0300

    added readme.md and requirements.txt

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# predict_number_game
+# My data_science project
+
+## Projects
+
+* [Project 0. Game 'Predict number']{https://github.com/TooezzE/predict_number_game}
---
+++ my_game.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+
+def score_game(random_predict) -> int:
+    """За какое количество попыток в среднем за 10000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(10000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(random_predict(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за: {score} попытки")
+    
+    
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    count = 0 
+    predict = np.random.randint(1, 101) 
+    prev_step = 50 
+ 
+    while number != predict: 
+        count += 1 
+        if number < predict: 
+            predict -= prev_step 
+        elif number > predict: 
+            predict += prev_step 
+         
+        prev_step = round(prev_step / 2, 0)
+    # Ваш код заканчивается здесь
+
+    return count
+
+
+if __name__ == "__main__":
+    print('Run benchmarking for game_core_v3: ', end='')
+    score_game(game_core_v3)
